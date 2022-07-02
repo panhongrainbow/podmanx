de461a7f768c486b1d0f36195f6ac6d47844f442
PanHongRainbow <panhongrainbow@gmail.com>
2022-07-02 15:41:14 +0800

    List containers by grabbing labels
    I have to check the containers exist by grabbing labels

--- examples/echo/echo_test.go
+++ examples/echo/echo_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/containers/podman/v3/pkg/bindings"
+	"github.com/containers/podman/v3/pkg/bindings/containers"
 	"github.com/containers/podman/v3/pkg/bindings/images"
 	"github.com/stretchr/testify/require"
 	"os"
@@ -11,6 +12,10 @@ import (
 )
 
 func TestEcho(t *testing.T) {
+
+	// >>>>> mimic "podman --version"
+	// >>>>> 相对于 "podman --version"
+
 	// create a client
 	// 创建一个客户端
 	sock_dir := os.Getenv("XDG_RUNTIME_DIR")
@@ -21,6 +26,9 @@ func TestEcho(t *testing.T) {
 		os.Exit(1)
 	}
 
+	// >>>>> mimic "podman image exists k8s.gcr.io/echoserver:1.4"
+	// >>>>> 相对于 "podman image exists k8s.gcr.io/echoserver:1.4"
+
 	// check the image exists
 	// 检查镜像是否存在
 	exists, err := images.Exists(conn, "k8s.gcr.io/echoserver:1.4", nil)
@@ -30,9 +38,15 @@ func TestEcho(t *testing.T) {
 	}
 	fmt.Println("image exists ?", exists)
 
-	// 下载镜像
-	// pull an image
+	// pull an image if it doesn't exist
+	// 如果不存在，重新下载镜像
 	if !exists {
+
+		// >>>>> mimic "podman image pull k8s.gcr.io/echoserver:1.4"
+		// >>>>> 相对于 "podman image pull k8s.gcr.io/echoserver:1.4"
+
+		// 下载镜像
+		// pull an image
 		_, err = images.Pull(conn, "k8s.gcr.io/echoserver:1.4", nil)
 		if err != nil {
 			fmt.Println(err)
@@ -52,5 +66,29 @@ func TestEcho(t *testing.T) {
 		require.Equal(t, true, exists)
 
 		fmt.Println("image exists after pull ?", exists)
+	}
+
+	// >>>>> mimic "podman ps --filter label=io.podman.compose.project=echo -a --format '{{ index .Labels "io.podman.compose.config-hash"}}'"
+	// >>>>> 相对于 "podman ps --filter label=io.podman.compose.project=echo -a --format '{{ index .Labels "io.podman.compose.config-hash"}}'"
+
+	// 启动计划是否已经存在
+	// check if the plan exists
+
+	listOptions := containers.ListOptions{
+		Filters: map[string][]string{
+			"label": {"io.podman.compose.project=echo"},
+		},
+	}
+
+	listContainer, err := containers.List(conn, &listOptions)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if len(listContainer) > 0 {
+		fmt.Println("plan exists")
+	} else {
+		fmt.Println("plan not exists")
 	}
 }
